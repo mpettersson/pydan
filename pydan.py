f2051cdc7120c7c422628f9e164326404eb70079
Hebrew Hammer <thehebrew@gmail.com>
2013-11-20 11:07:41 -0600

    Added verbose output, some more TODOs, and a few minor changes.

--- pydan.py
+++ pydan.py
@@ -1,5 +1,6 @@
-import argparse
+import os
 import signal
+import argparse
 from shodan import WebAPI
 import xml.etree.ElementTree as ET
 
@@ -11,10 +12,12 @@ def killme(signum = 0, frame = 0):
 
 def query(query, local=False):
     if local:
-        pass#TODO: Locally query XML data
+        verboseprint("performing a local query (\"",query,"\")")
+        #TODO: Locally query XML data
     else:
         print "Searching Shodan...",
         try:
+            verboseprint("submitting query to Shodan via webapi (\"",query,"\")")
             results = api.search(query)
         except Exception, e:
             print "Failed! (Error: %s)" % e
@@ -25,6 +28,7 @@ def query(query, local=False):
 def lookupHost(ip):
     print "Looking up host on Shodan...",
     try:
+        verboseprint("sumitting host (",ip,") query to Shodan via webapi")
         host = api.host(ip)
     except Exception, e:
         print "Failed! (Error: %s)" % e
@@ -43,17 +47,18 @@ def lookupHost(ip):
                 Banner: %s
 
         """ % (item['port'], item['banner'])
-    #TODO:Merge results into XML tree
+    #TODO:Merge results into XML tree (needs unified XML format)
 
 def findExploits(query):
     print "Searching for exploits...",
     try:
+        verboseprint("submitting exploit query to Shodan via webapi (\"",query,"\")")
         exploits = api.exploits.search(query)
     except Exception, e:
         print "Failed! (Error: %s)" % e
     
     print "Success!"
-    #TODO:Merge results into XML tree
+    #TODO:Merge results into XML tree (needs unified XML format)
 
 def formatFilename(fname):
     valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
@@ -61,12 +66,15 @@ def formatFilename(fname):
     filename = filename.replace(' ','_')
     if not filename.endswith(".xml"):
         filename += ".xml"
+    verboseprint("formatted output filename \"",fname,"\" to \"",filename,"\"")
     return filename
 
 def exportResults(tree, fname):
     tree.write(fname)
+    print "Wrote output to \"",fname,"\" successfullly!"
 
 def dictToXMLTree(dict):
+    verboseprint("importing query results to XML tree")
     global out_root
     for host in dict['matches']:
         out_root.append(ET.Element("host",host))
@@ -79,6 +87,8 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", dest = "ofname", type = argparse.FileType('w'), metavar="FILE", help = "Write output to FILE.", required = True)
     parser.add_argument("-x", "--xml", dest = "xml_file", type = argparse.FileType('r'), metavar="FILE", help = "Name of XML file to import and perform operations on locally.")
     parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help = "Verbose mode.")
+    #TODO:Option for "batch" file of queries?
+    #TODO:Make '-k' and '-x' mutually exclusive? Or combine results?
     
     #TODO:Mutually exclusive group? Or support multiple queries per run? (depends on unified XML format)
     group = parser.add_argument_group('Actions')
@@ -90,6 +100,15 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
+    if args.verbose:
+        def verboseprint(*args):
+            for arg in args:
+               print arg,
+            print
+        verboseprint("verbose mode activated")
+    else:
+        verboseprint = lambda *a: None
+    
     if not (args.query or args.host or args.exploit):
         parser.error("Not enough arguements given.")
     
@@ -100,22 +119,19 @@ if __name__ == "__main__":
         parser.error("Shodan API key required to perform queries.")
     
     if args.api_key:
+        verboseprint("api key detected")
         api = WebAPI(args.api_key)
-
-    if args.verbose:
-        def verboseprint(*args):
-            for arg in args:
-               print arg,
-            print
-    else:
-        verboseprint = lambda *a: None
+        verboseprint("webapi object created successfully")
     
     if args.xml_file and args.xml_file != "":
+        verboseprint("input XML file detected")
         tree = ET.parse(xml_file)
+        verboseprint("parsed xml file successfully")
     
     #TODO:Define a unified XML structure
     out_tree = ET.ElementTree(ET.Element("shodan"))
     out_root = tree.getroot()
+    verboseprint("initialized empty xml tree for output")
     
     if args.query:
         if args.xml_file:
