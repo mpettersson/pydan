0efc40d86cf90acc96a0ecd4fab4048fbb37277c
Hebrew Hammer <thehebrew@gmail.com>
2013-12-10 21:58:30 -0600

    Better implemention of fingerprinting
    Also:
     - updated some TODOs
     - better conformed to ElemenTree standards by using "get" method to
       access an element's attribute
     - convered some single quotes to double quotes (to be consistent)
     - finally learned how to make proper git commit messages

--- pydan.py
+++ pydan.py
@@ -6,6 +6,8 @@ from shodan import WebAPI
 import xml.etree.ElementTree as ET
 from collections import defaultdict
 
+#TODO: Apply good coding standards and implement some OOP
+
 class CustomArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
         super(CustomArgumentParser, self).__init__(*args, **kwargs)
@@ -18,13 +20,11 @@ class CustomArgumentParser(argparse.ArgumentParser):
                 break
             yield arg
 
-#TODO: Predefined queries?
-#TODO: Logic to correlate exploits to hosts/devices?
-
 #exit handler for signals.
 def killme(signum = 0, frame = 0):
     os.kill(os.getpid(), 9)
 
+#TODO:Check if xml elements already exist (for possible future upgrades)
 def query(out_tree, query, local=False):
     if local:
         verboseprint("performing a local query (\"",query,"\")")
@@ -77,7 +77,7 @@ def query(out_tree, query, local=False):
         
         verboseprint("importing query results to XML tree")
         out_query = ET.SubElement(out_tree,"query",{"query":query,"type":"api"})
-        out_hosts = ET.SubElement(out_query,"hosts")#add metdata to attrib?
+        out_hosts = ET.SubElement(out_query,"hosts")#retain metdata to attrib?
         for host in results["matches"]:
             out_hosts.append(ET.Element("host",host))
         
@@ -97,9 +97,9 @@ def lookupHost(out_tree, ip):
         IP: %s
         Country: %s
         City: %s
-    """ % (host['ip'], host.get("country", None), host.get("city", None)))
+    """ % (host["ip"], host.get("country", None), host.get("city", None)))
 
-    for item in host['data']:
+    for item in host["data"]:
         verboseprint("""
                 Port: %s
                 Banner: %s
@@ -108,7 +108,7 @@ def lookupHost(out_tree, ip):
     
     verboseprint("importing host info to XML tree")
     out_query = ET.SubElement(out_tree,"host_query",{"query":ip})
-    out_host = ET.SubElement(out_query,"host")
+    out_host = ET.SubElement(out_query,"hosts")
     out_host.append(ET.Element("host",host))
     
     return out_query
@@ -140,13 +140,20 @@ def formatFilename(fname):
     verboseprint("formatted output filename \"",fname,"\" to \"",filename,"\"")
     return filename
 
-def fingerprint(banner):
-    try:
-        matches = api.fingerprint(banner)
-    except Exception, e:
-        print "Failed! (Error: %s)" % e
+def fingerprint(out_query):
+    out_query_hosts = out_query.find('./hosts')
     
-    return matches
+    for host in out_query_hosts:
+        try:
+            verboseprint("submitting fingerprint request to Shodan via webapi")
+            results = api.fingerprint(host[0].text)
+        except Exception, e:
+            print "Failed! (Error: %s)" % e
+        
+        if results["matches"]:
+            fingerprints = ET.SubElement(host,"fingerprints")
+            for fingerprint in results["matches"]:
+                fingerprints.append("fingerprint",fingerprint[0])
 
 def enumServers(out_query):
     out_query_hosts = out_query.find('./hosts')
@@ -171,7 +178,7 @@ def lookupServerExploits(out_servers):
     for server in out_servers:
         try:
             verboseprint("submitting exploit query to Shodan via webapi (\"",query,"\")")
-            exploits = api.exploitdb.search(server.attrib["name"])
+            exploits = api.exploitdb.search(server.get("name"))
         except Exception, e:
             print "Failed! (Error: %s)" % e
         if exploits and not exploits["error"]:
@@ -215,11 +222,11 @@ vulnerable devices.
     parser.add_argument("-k", "--key", dest = "api_key", metavar="KEY", help = "Shodan API Key.")
     parser.add_argument("-o", "--output", dest = "ofname", type = argparse.FileType('w'), metavar="FILE", help = "write output to FILE", required = True)
     parser.add_argument("-x", "--xml", dest = "xml_file", type = argparse.FileType('r'), metavar="FILE", help = "name of XML file to import and perform operations on locally")
+    parser.add_argument("--fingerprint", action="store_true", dest="fingerprint", help = "(experimental) attempt to fingerprint devices based on their banner's")
     parser.add_argument("--xlookup", action="store_true", dest="xlookup", help = "attempt to find exploits on the types of devices found")
     parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help = "verbose mode")
     
-    #TODO:Option for "batch" file of queries?
-    #TODO:Make '-k' and '-x' mutually exclusive? Or combine results?
+    #with '-k' and '-x', allow api query too and combine results?
     
     group = parser.add_argument_group(title='Actions (mutually exclusive)')
     actions = group.add_mutually_exclusive_group(required=True)
@@ -271,10 +278,14 @@ vulnerable devices.
         else:
             out_query = query(out_tree, args.query)
         out_servers = enumServers(out_query)
+        if args.fingerprint:
+                pass#which hosts to fingerprint? All seems too much (depending on query)
         if args.xlookup:
             out_server_exploits = lookupServerExploits(out_servers)
     if args.host:
         out_query = lookupHost(out_tree, args.host)
+        if args.fingerprint:
+            fingerprint(out_query)
     if args.exploit:
         out_query = findExploits(out_tree, args.exploit)
     
